70879f3f20c4d755aaa32cc9565125eebd63d8b1
Deepthi Purijala <saideepthi.p@veltris.com>
2024-09-10 11:15:59 +0530

    relationship models

--- DjangoSample/app1/admin.py
+++ DjangoSample/app1/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
-from .models import ChaiVariety
+from .models import ChaiVariety, ChaiReview, Store, ChaiCertificate
 
 # Register your models here.
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    # list_display is a tuple
+    list_display = ('name', 'type', 'date_added')
+    inlines = [ChaiReviewInline]
+    # inlines: This is an attribute used in Django's ModelAdmin class to specify related models that should be displayed inline (i.e., within the same form) when editing or adding an instance of the main model.
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('ChaiVarieties',)
+    # filter_horizontal: This is an admin-specific setting that provides a better interface for managing many-to-many relationships. 
+    # By default, Django admin displays many-to-many relationships with a dual listbox interface (i.e., a pair of select boxes where you move items from one to another).
+    # Using filter_horizontal changes this to a widget with a horizontal filter, which makes it easier to select and manage related objects.
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'number')
+
 # register the model in DB
-admin.site.register(ChaiVariety)
+admin.site.register(ChaiVariety, ChaiVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
--- DjangoSample/app1/models.py
+++ DjangoSample/app1/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ChaiVariety(models.Model):
@@ -20,3 +21,38 @@ class ChaiVariety(models.Model):
     # to modify the display of object(records) added in the model
     def __str__(self):
         return self.name
+    
+# one to many relationship
+class ChaiReview(models.Model):
+    chai = models.ForeignKey(ChaiVariety, on_delete=models.CASCADE, related_name='reviews')
+    # foreign key arguments - model name
+    # ON DELETE CASCADE is used to specify that when a row is deleted from the parent table, all rows in the child table that reference the deleted row should also be deleted.
+    # This is useful for maintaining the integrity of the database.
+    # related name represents the name of the current table that needs to be inserted in the other table for which we are establishing the relationship 
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) :
+        return f'{self.user.userName} review for {self.chai.name}'
+    
+
+# many to many relationship
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    ChaiVarieties = models.ManyToManyField(ChaiVariety, related_name="stores")
+
+    def __str__(self) :
+        return self.name
+    
+# one to one relationship
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVariety,on_delete=models.CASCADE, related_name="certificate")
+    number = models.CharField(max_length=100)
+    issuedDate = models.DateTimeField(default=timezone.now)
+    validUntil = models.DateTimeField()
+
+    def __str__(self):
+        return f'Certificate for {self.chai.name}'
